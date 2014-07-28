812b529d9c32879327306013ed908d3a28c86d32
Mark Knutson <mark.knutson@C02LX26EF6T6.local>
2014-07-28 13:47:45 -0500

    will pivot and sort

--- lib/csv_pivot/pivot_table.rb
+++ lib/csv_pivot/pivot_table.rb
@@ -1,16 +1,15 @@
 require 'csv'
-require 'set'
 
 module CsvPivot
   class PivotTable
 
     DEFAULT_OPTIONS = {
-      :headers      => true,
-      :sort         => true,
-      :sort_on      => 0,
-      :aggregation  => "sum", #sum, average, standard deviation, count
-      :total_column => false,
-      :total_row    => false
+      :headers          => true,
+      :sort             => false,
+      :sort_on          => 0, 
+      :total_column     => false,
+      :total_row        => false,
+      :output_file      => nil
     }
 
     def initialize(opts = {})
@@ -20,75 +19,113 @@ module CsvPivot
       @pivot_columns = @opts[:pivot_columns]
       @pivot_data    = @opts[:pivot_data]
       @sort          = @opts[:sort]
-
+      @output_file   = @opts[:output_file]
       @headers       = @opts[:headers]
-      
-      create_header_row_and_label_column
+      @method        = @opts[:aggregate_method]
 
-      create_data_hash
-      populate_data_hash
+      p = Proc.new do |array|  # the default aggregation method: sum
+        array.map(&:to_i).reduce(0, :+)
+      end
 
-      aggregate_data
+      @method      ||= p
+
+      create_data_store
+      aggregate_data 
+      map_columns_and_rows
+      sort if @sort
+      create_table
+
+      output_csv if @output_file
+
     end
 
     def pivot
-      create_table
+      @pivoted_table
     end
 
-    def create_header_row_and_label_column
-      @header_row   = Hash.new
-      @label_column = Hash.new
-      @header_row.store(@pivot_rows, 0)#header for label column
-      i = j = 1  #This is so un-rubyish?
+    def create_data_store
+      @data_store = Hash.new
       CSV.foreach(@input_path, :headers => @headers) do |row|
-        if !@header_row.include? row[@pivot_columns]  then
-          @header_row.store( row[@pivot_columns], i )
-          i += 1
+        if @data_store.include? "#{row[@pivot_rows]}:#{row[@pivot_columns]}" then
+          @data_store["#{row[@pivot_rows]}:#{row[@pivot_columns]}"][:data].push(row[@pivot_data])
+        else
+          @data_store.store("#{row[@pivot_rows]}:#{row[@pivot_columns]}",
+                            {:row    => row[@pivot_rows],
+                             :column => row[@pivot_columns],
+                             :data   => [row[@pivot_data]]} )
         end
-        if !@label_column.include? row[@pivot_rows]  then
-          @label_column.store(row[@pivot_rows], j)
-          j += 1
-        end
-      end
-    end
-
-    def create_data_hash
-      @data_hash = Hash.new
-      @label_column.each_key do |row|
-        @header_row.each_key do |column|
-          @data_hash.store("#{row} #{column}", {:row => row, :column => column, :data => Array.new})
-        end
-      end
-    end
-
-    def populate_data_hash
-      CSV.foreach(@opts[:input_path], :headers => @opts[:headers]) do |row|
-        @data_hash["#{row[@pivot_rows]} #{row[@pivot_columns]}"][:data].push(row[@pivot_data].to_f)
       end
     end
 
     def aggregate_data
-      @data_hash.each do |key, value|
-        puts "#{key}:  #{value.inspect}"
-        value[:data] = value[:column] if value[:column] == @pivot_columns
-        value[:data] = value[:column] if value[:column] == @pivot_rows
-        value[:data] = value[:data].inject(0, :+) if value[:column] != @pivot_columns || value[:column] != @pivot_rows
+      @data_store.each_value do |value|
+        value[:data] = @method.call(value[:data])
+      end
+    end
+
+    def map_columns_and_rows
+      @column_map = Hash.new
+      @row_map    = Hash.new
+      col_i = row_i = 1
+      @data_store.each_value do |value|
+        if !@column_map.include? value[:column]
+          @column_map.store(value[:column], col_i) 
+          col_i += 1
+        end
+        if !@row_map.include? value[:row]
+          @row_map.store(value[:row], row_i) 
+          row_i += 1
+        end
+      end
+    end
+
+    def sort
+      sorted_columns = @column_map.keys.sort
+      sorted_rows    = @row_map.keys.sort
+      sorted_columns.each_with_index do |column, index|
+        @column_map[column] = index + 1
+      end
+      sorted_rows.each_with_index do |row, index|
+        @row_map[row] = index + 1
       end
     end
 
     def create_table
-      @pivoted_table = Array.new
-      @pivoted_table.push(@header_row.keys)
-      @label_column.each do |label, index|
-        @pivoted_table[index] = [label]
+      @pivoted_table = [[]]
+      @column_map.each do |key, value|
+        @pivoted_table[0][value] = key
       end
-      @data_hash.each_value do |value|
-        row = @label_column[value[:row]]
-        column = @header_row[value[:column]]
-        @pivoted_table[row][column] = value[:data] if column != @header_row[@pivot_rows]
+      @row_map.each do |key, value|
+        @pivoted_table[value] = [key]
       end
+      @data_store.each_value do |value|
+        row    = @row_map[value[:row]]
+        column = @column_map[value[:column]]
+        @pivoted_table[row][column] = value[:data]
+      end
+      @pivoted_table[0][0] = @pivot_rows if @headers
       @pivoted_table
+    end
+
+    def output_csv
+      CSV.open(@output_path, "w") do |csv|
+        @pivoted_table.each do |row|
+          csv << row
+        end
+      end
     end
 
   end # end class
 end
+
+=begin
+input = {:input_path => "/Users/mark.knutson/Documents/csv_pivot/spec/fixtures/license_server_record_history_by_regkey.csv", 
+                     :pivot_rows => "month",    # group by
+                     :pivot_columns => "limited_licensed_seats", # new headers
+                     :pivot_data => "registration_key",
+                     :output_file => "/Users/mark.knutson/Documents/csv_pivot/spec/fixtures/license_server_record_history_by_regkey_pivoted.csv",
+                     :sort => false}
+CsvPivot::PivotTable.new(input)
+=end
+
+
